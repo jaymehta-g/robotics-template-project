80c2ed2e4c9a713fff16ac66d558d7449c83f6d2
Jay Mehta <91770194+jaymehta-g@users.noreply.github.com>
2022-10-25 16:38:19 -0500

    remove invalid imports

--- RobotTemplateProject/src/main/java/frc/robot/RobotContainer.java
+++ RobotTemplateProject/src/main/java/frc/robot/RobotContainer.java
@@ -6,12 +6,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.DriveForwardCommand;
-import frc.robot.commands.ElevatorUpCommand;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.DriveTrainSystem;
-import frc.robot.subsystems.ElevatorSystem;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.DriveTrainCommand;
+import frc.robot.subsystems.DriveTrainSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -22,15 +18,8 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  private final DriveTrainSystem m_driveTrainSystem = new DriveTrainSystem();
-
-  private final ElevatorSystem m_elevatorSystem = new ElevatorSystem();
-
-  private final DriveForwardCommand m_driveForwardCommand = new DriveForwardCommand(m_driveTrainSystem);
-
-  private final ElevatorUpCommand m_elevatorUpCommand = new ElevatorUpCommand(m_elevatorSystem);
+  private final DriveTrainSubsystem m_driveTrainSystem = new DriveTrainSubsystem();
   
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
