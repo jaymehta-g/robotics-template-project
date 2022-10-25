3dc78ac15078ee4f3ac9ee7260b4da613d4351aa
Jay Mehta <91770194+jaymehta-g@users.noreply.github.com>
2022-10-25 16:42:17 -0500

    fin command

--- RobotTemplateProject/src/main/java/frc/robot/commands/DriveTrainCommand.java
+++ RobotTemplateProject/src/main/java/frc/robot/commands/DriveTrainCommand.java
@@ -1,21 +1,32 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.OI;
 import frc.robot.subsystems.DriveTrainSubsystem;
+
+import frc.robot.Constants.Axes;
+import frc.robot.Constants.Buttons;
 
 public class DriveTrainCommand extends CommandBase {
     private final DriveTrainSubsystem m_subsystem;
-    public DriveTrainCommand(DriveTrainSubsystem sub) {
+    private final OI m_oi;
+    public DriveTrainCommand(DriveTrainSubsystem sub, OI oi) {
         m_subsystem = sub;
         addRequirements(m_subsystem);
+
+        m_oi=oi;
     }
     @Override
     public void initialize() {
-
+        m_subsystem.stop();
     }
     @Override
     public void execute() {
-
+        m_subsystem.arcadeDrive(m_oi.getAxis(0, Axes.LEFT_STICK_Y), m_oi.getAxis(0, Axes.LEFT_STICK_X));
+    }
+    @Override
+    public void end(boolean interrupted) {
+        m_subsystem.stop();
     }
     @Override
     public boolean isFinished() {
