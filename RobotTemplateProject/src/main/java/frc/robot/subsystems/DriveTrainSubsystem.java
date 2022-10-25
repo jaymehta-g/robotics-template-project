14c304db77e23a3499afa9888f3cc2e49e7ee68d
Jay Mehta <91770194+jaymehta-g@users.noreply.github.com>
2022-10-25 16:23:07 -0500

    y

--- RobotTemplateProject/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ RobotTemplateProject/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -7,10 +7,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class DriveTrainSubsystem extends SubsystemBase{
-    public static final WPI_VictorSPX FRONT_LEFT; 
-    public static final WPI_VictorSPX FRONT_RIGHT;
-    public static final WPI_VictorSPX BACK_LEFT; 
-    public static final WPI_VictorSPX BACK_RIGHT;
+    private final WPI_VictorSPX FRONT_LEFT, FRONT_RIGHT, BACK_LEFT, BACK_RIGHT;
     public DriveTrainSubsystem() {
         FRONT_LEFT = new WPI_VictorSPX(Constants.DRIVE_TRAIN_FRONT_LEFT);
     }
