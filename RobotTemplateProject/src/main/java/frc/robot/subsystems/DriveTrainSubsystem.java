333089d1be7d2dbd4c68e1bf8806f446a14dab9a
Jay Mehta <91770194+jaymehta-g@users.noreply.github.com>
2022-10-25 16:38:05 -0500

    fin subsys

--- RobotTemplateProject/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ RobotTemplateProject/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -4,6 +4,7 @@ import frc.robot.Constants;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class DriveTrainSubsystem extends SubsystemBase{
@@ -14,9 +15,33 @@ public class DriveTrainSubsystem extends SubsystemBase{
         FRONT_RIGHT = new WPI_VictorSPX(Constants.DRIVE_TRAIN_FRONT_RIGHT);
         BACK_LEFT   = new WPI_VictorSPX(Constants.DRIVE_TRAIN_BACK_LEFT);
         BACK_RIGHT  = new WPI_VictorSPX(Constants.DRIVE_TRAIN_BACK_RIGHT);
+
+        FRONT_LEFT  .setNeutralMode(NeutralMode.Brake);
+        FRONT_RIGHT .setNeutralMode(NeutralMode.Brake);
+        BACK_LEFT   .setNeutralMode(NeutralMode.Brake);
+        BACK_RIGHT  .setNeutralMode(NeutralMode.Brake);
+
         BACK_LEFT.follow(FRONT_LEFT);
         BACK_RIGHT.follow(FRONT_RIGHT);
+
         DIFFERENTIAL_DRIVE = new DifferentialDrive(FRONT_LEFT,FRONT_RIGHT);
     }
-    
+    @Override
+    public void periodic() {
+
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        
+    }
+    public void stop() {
+        FRONT_LEFT  .set(0);
+        FRONT_RIGHT .set(0);
+        BACK_LEFT   .set(0);
+        BACK_RIGHT  .set(0);
+    }
+    public void arcadeDrive(double s, double r) {
+        DIFFERENTIAL_DRIVE.arcadeDrive(s, r);
+    }
 }
