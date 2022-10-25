c568f4933da0fb1770c3a0bd28ef6a06d7972fbd
Jay Mehta <91770194+jaymehta-g@users.noreply.github.com>
2022-10-25 16:15:46 -0500

    added subsys

---
+++ RobotTemplateProject/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -0,0 +1,11 @@
+package main.java.frc.robot.subsystems;
+
+public class DriveTrainSubsystem extends SubsystemBase{
+    WPI_VictorSPX FRONT_LEFT; 
+    WPI_VictorSPX FRONT_RIGHT;
+    WPI_VictorSPX BACK_LEFT; 
+    WPI_VictorSPX BACK_RIGHT;
+    public DriveTrainSubsystem() {
+
+    }
+}
